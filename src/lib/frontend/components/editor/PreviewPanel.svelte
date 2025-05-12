21785a368e1d5b17d9aa7469fb15d1c5f3fb7c40
LeqitDev <markus.hamacher16@gmail.com>
2025-05-12 13:35:59 +0200

    feat: enhance SVG rendering in PreviewPanel; improve layout and zoom handling for better display

--- src/lib/frontend/components/editor/PreviewPanel.svelte
+++ src/lib/frontend/components/editor/PreviewPanel.svelte
@@ -5,26 +5,60 @@
 	import RendererWorker from '@/lib/backend/worker/renderer?worker';
 	import type { Renderer as RendererType } from '@/lib/backend/worker/renderer/renderer';
 	import eventController from '@/lib/backend/events';
+	import { debug } from '@/lib/backend/utils';
 
 	const editorManager = getEditorManager();
 	let canvasContainer: HTMLDivElement;
-	
+	const pages: {
+		normalDimensions: {
+			width: number;
+			height: number;
+		};
+		previewElement: HTMLImageElement;
+		previewContainer: HTMLDivElement;
+	}[] = [];
+
+	let zoom = $state(1);
 
 	function renderCompilationResult(output: Output) {
 		let cur_count = canvasContainer.childElementCount;
 		if ('Html' in output) return;
-		for (const [i, page] of output.Svg.entries()) {
-			const child = canvasContainer.children[i];
-			if (child) {
-				const base = btoa(page);
-				(child as HTMLImageElement).src = `data:image/svg+xml;base64,${base}`;
+		for (const [i, svg] of output.Svg.entries()) {
+			const page = pages.at(i);
+			// get viewbox of svg page
+			const viewBox = svg.match(/viewBox="([^"]+)"/);
+			if (!viewBox) {
+				debug('warning', 'Renderer', 'No viewBox found in SVG page. Skipping...');
+				continue;
+			}
+			if (page) {
+				const base = btoa(svg);
+				page.previewElement.src = `data:image/svg+xml;base64,${base}`;
+
+				page.previewContainer.style.width = `${page.normalDimensions.width * zoom}px`;
+				page.previewContainer.style.height = `${page.normalDimensions.height * zoom}px`;
+				page.previewElement.width = page.normalDimensions.width * zoom;
 			} else {
 				const img = document.createElement('img');
-				img.setAttribute('typst-page', i.toString());
-				const base = btoa(page);
+				const base = btoa(svg);
 				img.src = `data:image/svg+xml;base64,${base}`;
-				img.width = 5000;
-				canvasContainer.appendChild(img);
+				const [x, y, width, height] = viewBox[1].split(' ').map(Number);
+				img.width = width * zoom;
+				const div = document.createElement('div');
+				div.setAttribute('typst-page', i.toString());
+				div.style.width = `${width * zoom}px`;
+				div.style.height = `${height * zoom}px`;
+				div.appendChild(img);
+				canvasContainer.appendChild(div);
+				const normalDimensions = {
+					width: width,
+					height: height,
+				};
+				pages.push({
+					normalDimensions,
+					previewElement: img,
+					previewContainer: div,
+				});
 			}
 			/* if (i < cur_count) {
 				editorManager.renderer.update(i, page);
@@ -51,9 +85,9 @@
 	});
 </script>
 
-<div class="overflow-auto h-full">
+<div class="overflow-auto h-full flex justify-center-safe p-3">
 	<div
 		bind:this={canvasContainer}
-		class="grid items-center-safe justify-center-safe w-max gap-3 p-3"
+		class="grid items-center-safe justify-center-safe w-max h-max gap-3"
 	></div>
 </div>
